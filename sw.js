89aabc8faef5d4b1444b361def6dd2535bbb9f6f
massimo984 <massimo984@gmail.com>
2026-07-10 19:02:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'piano-piano-v3';
+const CACHE = 'piano-piano-v4';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
