5054173b28043948d189e08c8551310df30b5ae8
massimo984 <massimo984@gmail.com>
2026-07-15 15:47:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gameover-v8';
+const CACHE = 'gameover-v9';
 const FILES = ['./', './index.html', './allergia.html', './pianopiano.html', './manifest.json',
                './naso.png', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => {
